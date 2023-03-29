7d333f429019af37b149024e66b1d856444aa0ef
Maciej-Mudita <maciej.gibowicz@mudita.com>
2023-03-29 16:56:53 +0200

    update sysTick

--- module-bsp/board/rt1051/bellpx/bsp/lpm/WfiController.cpp
+++ module-bsp/board/rt1051/bellpx/bsp/lpm/WfiController.cpp
@@ -2,14 +2,29 @@
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #include "WfiController.hpp"
+#include <cstdint>
 #include <fsl_common.h>
+#include <Utils.hpp>
+#include <time/time_constants.hpp>
+#include <FreeRTOS.h>
+#include <task.h>
 
 namespace bsp
 {
+    static inline std::uint32_t HighFrequencyTimerTicksToMs(std::uint32_t ticks)
+    {
+        return ticks / (configRUN_TIME_STATS_CLOCK_HZ / utils::time::milisecondsInSecond);
+    }
+
     void enterWfiMode()
     {
+        const auto enterWfiTimerTicks = ulHighFrequencyTimerTicks();
         __DSB();
         __WFI();
         __ISB();
+        const auto exitWfiTimerTicks = ulHighFrequencyTimerTicks();
+        const auto sleepTimeMs =
+            HighFrequencyTimerTicksToMs(utils::computeIncrease(exitWfiTimerTicks, enterWfiTimerTicks));
+        xTaskCatchUpTicks(sleepTimeMs);
     }
 } // namespace bsp
