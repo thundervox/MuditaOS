31fa9d510bb1bee906c7ed1af18c89229d067332
Dawid Wojtas <dawid.wojtas@mudita.com>
2023-03-30 08:37:56 +0200

    Change brownout detection for WFI mode

--- module-bsp/board/rt1051/bellpx/brownout.cpp
+++ module-bsp/board/rt1051/bellpx/brownout.cpp
@@ -9,20 +9,17 @@ namespace bsp
 {
     namespace
     {
-        constexpr std::uint32_t OutputVoltage1P1 = 0x19; // 1.275V
         constexpr std::uint32_t OutputVoltage2P5 = 0x1F; // 2.875V
-
-        constexpr std::uint32_t OffsetVoltage1P1 = 0x05; // 5*25mv
-        constexpr std::uint32_t OffsetVoltage2P5 = 0x03; // 3*25mv
+        constexpr std::uint32_t OffsetVoltage2P5 = 0x03; // 3*25mV
     }                                                    // namespace
 
     void Brownout_init()
     {
-        // Config LDO Regulatorsand config Brownout voltage offsets
-        PMU_1P1EnableBrownout(PMU, true);
-        PMU_1P1SetRegulatorOutputVoltage(PMU, OutputVoltage1P1);
-        PMU_1P1SetBrownoutOffsetVoltage(PMU, OffsetVoltage1P1);
-        PMU_1P1EnableOutput(PMU, true);
+        // Config LDO Regulators and config Brownout voltage offsets
+        // PMU_1P1EnableBrownout(PMU, true);
+        // PMU_1P1SetRegulatorOutputVoltage(PMU, OutputVoltage1P1);
+        // PMU_1P1SetBrownoutOffsetVoltage(PMU, OffsetVoltage1P1);
+        // PMU_1P1EnableOutput(PMU, true);
 
         PMU_2P5nableBrownout(PMU, true);
         PMU_2P5SetRegulatorOutputVoltage(PMU, OutputVoltage2P5);
