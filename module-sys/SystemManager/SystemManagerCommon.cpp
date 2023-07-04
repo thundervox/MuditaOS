a4a2f3100f65269c058213c675d42cf44e0dad93
Maciej Gibowicz <maciej.gibowicz@mudita.com>
2023-07-04 10:01:15 +0200

    Changing CPU frequency timer to "Single shot timer"

--- module-sys/SystemManager/SystemManagerCommon.cpp
+++ module-sys/SystemManager/SystemManagerCommon.cpp
@@ -236,7 +236,7 @@ namespace sys
         // Start System manager
         StartService();
 
-        freqTimer = sys::TimerFactory::createPeriodicTimer(
+        freqTimer = sys::TimerFactory::createSingleShotTimer(
             this, "cpuTick", constants::timerInitInterval, [this](sys::Timer &) { FreqUpdateTick(); });
         freqTimer.start();
 
@@ -767,6 +767,7 @@ namespace sys
         auto ret = powerManager->UpdateCpuFrequency();
         cpuStatistics->TrackChange(ret);
         powerManager->EnterWfiIfReady();
+        freqTimer.restart(constants::timerPeriodInterval);
     }
 
     void SystemManagerCommon::UpdateResourcesAfterCpuFrequencyChange(bsp::CpuFrequencyMHz newFrequency)
