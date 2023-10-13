22b95fdfb95dfc50a0d621a1ba5db555fff50442
Lefucjusz <marcin.lyda@mudita.com>
2023-10-13 11:35:35 +0200

    Add power gating

--- module-bsp/board/rt1051/bellpx/bsp/lpm/WfiController.cpp
+++ module-bsp/board/rt1051/bellpx/bsp/lpm/WfiController.cpp
@@ -15,6 +15,8 @@
 #include <magic_enum.hpp>
 #include <log/log.hpp>
 
+#include <fsl_gpc.h>
+
 namespace bsp
 {
     namespace
@@ -72,12 +74,93 @@ namespace bsp
                     if (name == ANATOP_EVENT0_IRQn) {
                         const std::uint32_t status = PMU_GetStatusFlags(PMU);
                         LOG_INFO("ANATOP_EVENT0_IRQn: 0x%lx", status);
-                        NVIC_ClearPendingIRQ(name);
+                        //                        NVIC_ClearPendingIRQ(name);
                     }
                 }
             }
         }
 
+        void setWaitModeConfig()
+        {
+            /* Clear the SLEEPDEEP bit to go into sleep mode (WAIT) */
+            SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk;
+
+            /*
+             * ERR007265: CCM: When improper low-power sequence is used,
+             * the SoC enters low power mode before the ARM core executes WFI.
+             *
+             * Software workaround:
+             * 1) Software should trigger IRQ #41 (GPR_IRQ) to be always pending
+             *      by setting IOMUXC_GPR_GPR1_GINT.
+             * 2) Software should then unmask IRQ #41 in GPC before setting CCM
+             *      Low-Power mode.
+             * 3) Software should mask IRQ #41 right after CCM Low-Power mode
+             *      is set (set bits 0-1 of CCM_CLPCR).
+             */
+
+            IOMUXC_GPR->GPR1 |= IOMUXC_GPR_GPR1_GINT_MASK; // TODO this can be done once
+            __DSB();
+
+            GPC_EnableIRQ(GPC, GPR_IRQ_IRQn);
+
+            const auto clpcr = CCM->CLPCR & (~(CCM_CLPCR_LPM_MASK | CCM_CLPCR_ARM_CLK_DIS_ON_LPM_MASK));
+            CCM->CLPCR       = clpcr | CCM_CLPCR_MASK_L2CC_IDLE_MASK | CCM_CLPCR_MASK_SCU_IDLE_MASK |
+                         CCM_CLPCR_BYPASS_LPM_HS0_MASK | CCM_CLPCR_BYPASS_LPM_HS1_MASK | CCM_CLPCR_STBY_COUNT_MASK |
+                         CCM_CLPCR_ARM_CLK_DIS_ON_LPM_MASK | CCM_CLPCR_LPM(kCLOCK_ModeWait);
+
+            GPC_DisableIRQ(GPC, GPR_IRQ_IRQn);
+        }
+
+        void setRunModeConfig()
+        {
+            CCM->CLPCR &= ~(CCM_CLPCR_LPM_MASK | CCM_CLPCR_ARM_CLK_DIS_ON_LPM_MASK);
+        }
+
+        constexpr auto CCMGateRegsCount = 7;
+        volatile std::uint32_t clockGateValues[CCMGateRegsCount];
+
+        void setLowPowerClockGate()
+        {
+            /* Save of the clock gate registers */
+            clockGateValues[0] =
+                CCM->CCGR0; /* TODO all these additional clock disabling should be moved to clock_config.cpp */
+            clockGateValues[1] = CCM->CCGR1 & ~(CCM_CCGR1_CG5(3));                      // Disable enet
+            clockGateValues[2] = CCM->CCGR2 & ~(CCM_CCGR2_CG15(3) | CCM_CCGR2_CG14(3)); // Disable pxp and lcd
+            clockGateValues[3] = CCM->CCGR3 & ~(CCM_CCGR3_CG13(3) | CCM_CCGR3_CG12(3) | CCM_CCGR3_CG11(3) |
+                                                CCM_CCGR3_CG10(3)); // Disable acmps
+            clockGateValues[4] = CCM->CCGR4 & ~(CCM_CCGR4_CG15(3) | CCM_CCGR4_CG14(3) | CCM_CCGR4_CG13(3) |
+                                                CCM_CCGR4_CG12(3)); // Disable encs
+            clockGateValues[5] =
+                CCM->CCGR5 & ~(CCM_CCGR5_CG4(3)); // Disable kpp, TODO all wdogs have enabled clocks, is it needed?
+            clockGateValues[6] = CCM->CCGR6;
+
+            /* Set low power gate values */
+            CCM->CCGR0 = CCM_CCGR0_CG0(1) | CCM_CCGR0_CG1(1) | CCM_CCGR0_CG3(3) | CCM_CCGR0_CG11(1) | CCM_CCGR0_CG12(1);
+            CCM->CCGR1 = CCM_CCGR1_CG9(3) | CCM_CCGR1_CG10(3) /* was 1, GPT1 is used in wait mode as timebase */ |
+                         CCM_CCGR1_CG13(1) | CCM_CCGR1_CG14(1) | CCM_CCGR1_CG15(1);
+            CCM->CCGR2 = CCM_CCGR2_CG2(1) | CCM_CCGR2_CG8(1) | CCM_CCGR2_CG9(1) | CCM_CCGR2_CG10(1);
+            CCM->CCGR3 = CCM_CCGR3_CG2(3) /* was 1, but SEMC clock needs to be enabled */ | CCM_CCGR3_CG4(1) |
+                         CCM_CCGR3_CG9(1) | CCM_CCGR3_CG14(3) | CCM_CCGR3_CG15(1);
+            CCM->CCGR4 = CCM_CCGR4_CG1(1) | CCM_CCGR4_CG2(1) | CCM_CCGR4_CG4(1) | CCM_CCGR4_CG5(1) | CCM_CCGR4_CG6(1) |
+                         CCM_CCGR4_CG7(1);
+            CCM->CCGR5 = CCM_CCGR5_CG0(1) | CCM_CCGR5_CG1(1) | CCM_CCGR5_CG4(1) | CCM_CCGR5_CG6(1) | CCM_CCGR5_CG12(1) |
+                         CCM_CCGR5_CG14(3) /* was 1, but SNVS HP is needed for minute IRQ to work */ |
+                         CCM_CCGR5_CG15(1);
+            /* We can enable DCDC when need to config it and close it after configuration */
+            CCM->CCGR6 = CCM_CCGR6_CG3(1) | CCM_CCGR6_CG4(1) | CCM_CCGR6_CG5(1) | CCM_CCGR6_CG9(1) | CCM_CCGR6_CG10(1) |
+                         CCM_CCGR6_CG11(1) | CCM_CCGR4_CG15(3); // CG15 TMR3 added for encoder to wakeup
+        }
+
+        void restoreLowPowerClockGate()
+        {
+            CCM->CCGR0 = clockGateValues[0];
+            CCM->CCGR1 = clockGateValues[1];
+            CCM->CCGR2 = clockGateValues[2];
+            CCM->CCGR3 = clockGateValues[3];
+            CCM->CCGR4 = clockGateValues[4];
+            CCM->CCGR5 = clockGateValues[5];
+            CCM->CCGR6 = clockGateValues[6];
+        }
     } // namespace
 
     void allowEnteringWfiMode()
@@ -106,41 +189,37 @@ namespace bsp
             return;
         }
 
+        checkAndClearPendingIrq();
+
+        CCM->CGPR |=
+            CCM_CGPR_INT_MEM_CLK_LPM_MASK; // See
+                                           // https://patchwork.kernel.org/project/linux-arm-kernel/patch/1471885400-9140-1-git-send-email-Anson.Huang@nxp.com/
+        __DSB();
+
+        setWaitModeConfig();
+        __DSB();
+
         const auto enterWfiTimerTicks = ulHighFrequencyTimerTicks();
 
-        checkAndClearPendingIrq();
-        peripheralEnterDozeMode();
-
-        /*
-         * ERR006223: CCM: Failure to resuem from wait/stop mode with power gating
-         *   Configure REG_BYPASS_COUNTER to 2
-         *   Enable the RBC bypass counter here to hold off the interrupts. RBC counter
-         *  needs to be no less than 2.
-         */
-        CCM->CCR = (CCM->CCR & ~CCM_CCR_REG_BYPASS_COUNT_MASK) | CCM_CCR_REG_BYPASS_COUNT(2);
-        CCM->CCR |= (CCM_CCR_OSCNT(0xAF) | CCM_CCR_COSC_EN_MASK | CCM_CCR_RBC_EN_MASK);
-
-        /* Now delay for a short while (3usec) at this point
-         * so a short loop should be enough. This delay is required to ensure that
-         * the RBC counter can start counting in case an interrupt is already pending
-         * or in case an interrupt arrives just as ARM is about to assert DSM_request.
-         */
-        SDK_DelayAtLeastUs(3, CLOCK_GetCpuClkFreq());
-
-        __disable_irq();
+        const volatile std::uint32_t savedPrimask = DisableGlobalIRQ();
         __DSB();
         __ISB();
 
-        /* Clear the SLEEPDEEP bit to go into sleep mode (WAIT) */
-        SCB->SCR &= ~SCB_SCR_SLEEPDEEP_Msk;
+        setLowPowerClockGate();
+        __DSB();
+        peripheralEnterDozeMode();
 
         __DSB();
         __WFI();
         __ISB();
 
         peripheralExitDozeMode();
+        restoreLowPowerClockGate();
+        setRunModeConfig();
+        __DSB();
 
-        __enable_irq();
+        EnableGlobalIRQ(savedPrimask);
+        __DSB();
         __NOP();
 
         /* Block WFI mode so that OS wakes up fully and goes to sleep only after
@@ -151,6 +230,7 @@ namespace bsp
         const auto sleepTimeMs =
             highFrequencyTimerTicksToMs(utils::computeIncrease(exitWfiTimerTicks, enterWfiTimerTicks));
         xTaskCatchUpTicks(cpp_freertos::Ticks::MsToTicks(sleepTimeMs));
+
         LOG_INFO("*** WFI OUT sleep time: %ld ***", sleepTimeMs);
     }
 } // namespace bsp
